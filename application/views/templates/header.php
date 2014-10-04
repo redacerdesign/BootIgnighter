711f60a3d1d98086ae547fcd95a7781043a240ee
ross-redacerdesign <ross@redacerdesign.co.uk>
2014-10-04 09:01:21 +0100

    Core CodeIgnighter, Bootstrap, jQuery and Shiv's HTML5 fix added and configured.

---
+++ application/views/templates/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+        
+        <!-- Load stylesheets -->
+        <link rel="stylesheet" href="/bootstrap/css/bootstrap.css" type="text/css">
+        <link rel="stylesheet" href="/bootstrap/css/bootstrap-theme.css" type="text/css">
+        <link rel="stylesheet" href="/css/style.css" type="text/css">   
+        
+        <!-- Load javascript -->
+        <script type="text/javascript" src="/js/jquery-1.11.1.min.js"></script>        
+        <script type="text/javascript" src="/bootstrap/js/bootstrap.js"></script>
+        <!--[if lt IE 9]>
+        <script type="text/javascript" src="/js/html5shiv.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="container">
+            
+        
